f28b8b01d1373282f83f7d29b3fb4fd18bd36c07
Emmanuel <contact@Emmanuel24394.com>
2018-10-28 20:06:39 -0600

    Update registro_usuario.php
    usuarios

---
+++ registro_usuario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<?php include "include/scripts.php"	 ?>
+	<title>Registro De Usuario</title>
+</head>
+<body>
+     <?php include "include/header.php" ?>
+
+	<section id="container">
+         <div class="form_register">
+		
+			<h1>Registro Usuario</h1>
+			<hr>
+			<div class="alert"></div>
+
+            <form>
+            	
+            	<label for="nombre">Nombre</label>
+            	<input type="text" name="nombre" id="nombre" placeholder="Nombre Completo">
+            	<label for="email">Correo Electronico</label>
+            	<input type="email" name="correo" id="correo" placeholder="Correo Electronico">
+            	<label for="usuario">Usuario</label>
+            	<input type="text" name="usuario" id="usuario" placeholder="Usuario">
+            	<label for="clave">Clave</label>
+            	<input type="password" name="clave" id="clave" placeholder="Clave de Acceso">
+            	<label for="rol">Tipo de Usuario</label>
+            	<select name="rol" id="rol">
+            		<option value="1">Administrador</option>
+            		<option value="2">Supervisor</option>
+            		<option value="3">Empleado</option>
+            	</select>
+            	<input type="submit" value="Crear Usuario" class="btn_save">
+            </form>   
+
+
+		</div>
+
+	</section>
+	<?php include "include/footer.php" ?>
+</body>
+</html>
