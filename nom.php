e589ef24d1e4bc6c3506e37b366f5f4ce8654de2
CGSian <44557935+CGSian@users.noreply.github.com>
2018-10-28 20:15:05 -0600

    llamada a base
    llamada a base

---
+++ nom.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once"conexion.php";
+        $sql = "SELECT * FROM conkpi";
+        $result = mysqli_query($conection,$sql);
+        while ($registro = mysqli_fetch_array($result)) {
+        ?>
+        <?php  echo $registro ["nombre"] ?>
+        <?php
+        }
+
+          ?>
