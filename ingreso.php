caf96cba4ae20e3125b0c87fd85cee819e1dd0f3
Fabiola <fabybu@hotmail.com>
2018-10-30 23:59:06 -0600

    actualizando

--- ingreso.php
+++ ingreso.php
@@ -1,5 +1,10 @@
 <?php 
+  $volver="";
+  if(isset($_POST['volver']))$volver=$_POST['volver'];
 
+  if ($volver) {
+  	header('location: admin.php');
+  }else{
 
    $alert = '';
    
@@ -8,15 +13,22 @@
    	{
    		$alert = 'Ingrese Datos';
    	}else{
-   		require_once"conexion.php";
+   		require_once"com/conexion.php";
 
    		$datos = $_POST['datos'];
-   		
-
+   	 
    		$query = mysqli_query($conection,"SELECT * FROM datos WHERE datos= '$datos'");
    		$result = mysqli_num_rows($query);
         $query_insert = mysqli_query($conection, "INSERT INTO datos(datos) VALUES ('$datos')");
+      
+
 }}
+
+ 
+
+
+}
+
  ?>
 
 <!DOCTYPE html>
@@ -25,16 +37,16 @@
 	<meta charset="UTF-8">
 	<title>Sistema de medicion de KPI</title>
 		
-
+</head><link rel="stylesheet" type="text/css" href="css1/style2.css">
 <body>
 
-   
+    <?php include "com/gra.php" ?>
     
 	<section id="container">
 
 		<form action="" method="post">
 
-			
+			<h3><?php include "com/nom.php" ?></p></h3>
 			
 
 			<input type="text" name="datos" placeholder="Datos">
@@ -42,6 +54,7 @@
 			<div class="alert"><?php echo isset($alert) ? $alert : '';  ?></div>
 			
 			<input type="submit" value="INGRESAR">
+			<input type="submit" name="volver" value="VOLVER">
 	
 			
 		</form>
---
+++ inkpi.php
@@ -0,0 +1,50 @@
+<?php include "com/elimi.php" ?>
+<?php include "com/edatos.php" ?>
+<?php 
+$alert = '';
+   if (!empty($_POST)){
+      if (empty($_POST['nombre']) || empty($_POST['min']) || empty($_POST['max']))
+      {
+         $alert = 'Los Datos Son Obligatorios';
+      }else{
+         require_once"com/conexion.php";
+         $nombre = $_POST['nombre'];
+         $min = $_POST['min'];
+         $max = $_POST['max'];
+
+         $query = mysqli_query($conection,"SELECT * FROM conkpi WHERE nombre='$nombre' OR min='$min' OR max='$max'");
+         $result = mysqli_num_rows($query);
+         $query_insert = mysqli_query($conection, "INSERT INTO conkpi(nombre,min,max) VALUES ('$nombre','$min','$max')");
+
+            header('location: ingreso.php');
+         
+      }
+
+   }
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sistema de medición de KPI</title>
+	<link rel="stylesheet" type="text/css" href="css1/stylekpi.css">	
+</head>
+<body>
+	<section id="container">
+
+		<form action="" method="post">
+
+			<h3>Crear KPI</h3>
+			
+
+         <input type="text" name="nombre" placeholder="Ingresar Nombre de KPI">
+			<input type="text" name="min" placeholder="Ingresar Dato Minimo">
+			<input type="text" name="max" placeholder="Ingresar Dato Maximo">
+         <div class="alert"><?php echo isset($alert) ? $alert : '';  ?></div>
+			<input type="submit" value="INGRESAR">
+
+		</form>
+
+    </section>
+</body>	
+</html>
